e98d0ba60b881379a8b5552e1be2e4149c914e17
Soulaimane ELManioui <elmanioui.soulaimane@gmail.com>
2020-06-22 00:52:34 +0100

    design changes

--- GestionDesServices/Page Services disponibles/ServicesDisponibles.aspx.cs
+++ GestionDesServices/Page Services disponibles/ServicesDisponibles.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using GestionDesServices.Page_Services_disponibles.Souspage;
 namespace GestionDesServices
 {
     public partial class DemandeDeService : System.Web.UI.Page
