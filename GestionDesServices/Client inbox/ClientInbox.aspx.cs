5f34771f003816d1a6d1fc52d34327e5fc5e6e15
abdellah khalfi <khalfiabdellah@hotmail.com>
2020-07-03 19:12:56 +0100

    client inbox

--- GestionDesServices/Client inbox/ClientInbox.aspx.cs
+++ GestionDesServices/Client inbox/ClientInbox.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
 namespace GestionDesServices.Client_inbox
 {
     public partial class ClientInbox : System.Web.UI.Page
@@ -16,7 +16,23 @@ namespace GestionDesServices.Client_inbox
             Session["reponse"] = Request.QueryString["reponse"].ToString();
             Response.Write(Session["Reponse"] +" " +Session["pres"]+" "+Session["haha"]
                 +"La demande est accepter");
-            //Response.Redirect("../Page Anonce Fournisseur/liste.aspx");
+            if (Session["reponse"].ToString() == "Refuser") 
+            {
+                Connecter cn = new Connecter();
+                cn.con.Open();
+                cn.cmd = new SqlCommand("delete from services where ID_service =" + int.Parse(Session["haha"].ToString()), cn.con);
+                int k = cn.cmd.ExecuteNonQuery();
+                if (k != 0)
+                {
+                    Response.Redirect("../Page Anonce Fournisseur/liste.aspx");
+                }
+            }
+            else
+            {
+                string nb = Session["haha"].ToString();
+                Response.Redirect("../Page Anonce Fournisseur/liste.aspx?agree="+nb);
+            }
+            
             
         }
     }
