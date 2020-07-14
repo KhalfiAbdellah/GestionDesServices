4facb2f20474aa4fe9b9f01781a4f80c0a320a9c
abdellah khalfi <khalfiabdellah@hotmail.com>
2020-07-14 22:14:38 +0100

    fixer enregistrer dans user interface

--- GestionDesServices/UI/User Profile/User Profile.aspx.cs
+++ GestionDesServices/UI/User Profile/User Profile.aspx.cs
@@ -16,55 +16,59 @@ namespace GestionDesServices.UI.User_Profile
         Connecter cn = new Connecter();
         protected void Page_Load(object sender, EventArgs e)
         {
-            cn.con.Open();
+            if (!IsPostBack)
+            {
+                cn.con.Open();
 
-            //hiding the message or prompt about the deleting the account
-            attempt.Visible = false;
+                //hiding the message or prompt about the deleting the account
+                attempt.Visible = false;
 
 
-            //NOM
-            cn.cmd = new SqlCommand("select nom_clt, prenom_clt from Clients where ID_clt=" + 4, cn.con);
-            nom_prenom.InnerText = (string)cn.cmd.ExecuteScalar();
+                //NOM
+                cn.cmd = new SqlCommand("select nom_clt, prenom_clt from Clients where ID_clt=" + 4, cn.con);
+                nom_prenom.InnerText = (string)cn.cmd.ExecuteScalar();
+
+
+                //NOM
+                cn.cmd = new SqlCommand("select nom_clt from Clients where ID_clt=" + 4, cn.con);
+                nom.Text = (string)cn.cmd.ExecuteScalar();
+                nom.ReadOnly = true;
+
+                //PRENOM
+                cn.cmd = new SqlCommand("select prenom_clt from Clients where ID_clt=" + 4, cn.con);
+                prenom.Text = (string)cn.cmd.ExecuteScalar();
+                prenom.ReadOnly = true;
+
+                //EMAIL
+                cn.cmd = new SqlCommand("select email_clt from Clients where ID_clt=" + 4, cn.con);
+                email.Text = (string)cn.cmd.ExecuteScalar();
+                email.ReadOnly = true;
+
+                //ADRESS
+                cn.cmd = new SqlCommand("select adresse_clt from Clients where ID_clt=" + 4, cn.con);
+                adress.Text = (string)cn.cmd.ExecuteScalar();
+                adress.ReadOnly = true;
+
+                //NUMERO DE TELEPHONE
+                cn.cmd = new SqlCommand("select numTel_clt from Clients where ID_clt=" + 4, cn.con);
+                tel.Text = Convert.ToString(cn.cmd.ExecuteScalar());
+                tel.ReadOnly = true;
+
+                //AGE
+                cn.cmd = new SqlCommand("select age_clt from Clients where ID_clt=" + 4, cn.con);
+                age.Text = Convert.ToString(cn.cmd.ExecuteScalar());
+                age.ReadOnly = true;
+
+                //MOT DE PASSE
+                cn.cmd = new SqlCommand("select mdp_clt from Clients where ID_clt=" + 4, cn.con);
+                mdp.Text = (string)cn.cmd.ExecuteScalar();
+                mdp.ReadOnly = true;
+
+                Enregistrer.Visible = false;
+                Annuler.Visible = false;
+                cn.con.Close();
+            }
             
-
-            //NOM
-            cn.cmd = new SqlCommand("select nom_clt from Clients where ID_clt="+4,cn.con);
-            nom.Text = (string)cn.cmd.ExecuteScalar();
-            nom.ReadOnly = true;
-
-            //PRENOM
-            cn.cmd = new SqlCommand("select prenom_clt from Clients where ID_clt=" + 4, cn.con);
-            prenom.Text = (string)cn.cmd.ExecuteScalar();
-            prenom.ReadOnly = true;
-
-            //EMAIL
-            cn.cmd = new SqlCommand("select email_clt from Clients where ID_clt=" + 4, cn.con);
-            email.Text = (string)cn.cmd.ExecuteScalar();
-            email.ReadOnly = true;
-
-            //ADRESS
-            cn.cmd = new SqlCommand("select adresse_clt from Clients where ID_clt=" + 4, cn.con);
-            adress.Text = (string)cn.cmd.ExecuteScalar();
-            adress.ReadOnly = true;
-
-            //NUMERO DE TELEPHONE
-            cn.cmd = new SqlCommand("select numTel_clt from Clients where ID_clt=" + 4, cn.con);
-            tel.Text = Convert.ToString(cn.cmd.ExecuteScalar());
-            tel.ReadOnly = true;
-
-            //AGE
-            cn.cmd = new SqlCommand("select age_clt from Clients where ID_clt=" + 4, cn.con);
-            age.Text = Convert.ToString(cn.cmd.ExecuteScalar());
-            age.ReadOnly = true;
-
-            //MOT DE PASSE
-            cn.cmd = new SqlCommand("select mdp_clt from Clients where ID_clt=" + 4, cn.con);
-            mdp.Text = (string)cn.cmd.ExecuteScalar();
-            mdp.ReadOnly = true;
-
-            Enregistrer.Visible = false;
-            Annuler.Visible = false;
-            cn.con.Close();
         }
 
        
@@ -95,7 +99,7 @@ namespace GestionDesServices.UI.User_Profile
         protected void enregistrer(object sender, EventArgs e)
         {
             cn.con.Open();
-            cn.cmd = new SqlCommand("update Clients set nom_clt='" + nom.Text + "', prenom_clt='" + prenom.Text + "', email_clt='" + email.Text + "', mdp_clt='" + mdp.Text + "', numTel_clt=" + Convert.ToInt32(tel.Text) + ", age_clt=" + Convert.ToInt32(age.Text) + " where ID_clt=" + 4 + "", cn.con);
+            cn.cmd = new SqlCommand("update Clients set nom_clt='" + nom.Text + "', prenom_clt='" + prenom.Text + "', email_clt='" + email.Text + "', mdp_clt='" + mdp.Text + "', numTel_clt=" + Convert.ToInt32(tel.Text) + ", age_clt=" + Convert.ToInt32(age.Text) + " where ID_clt=" + 4 , cn.con);
             cn.cmd.ExecuteNonQuery();
             Response.Write("<script>alert('Les modifications sont enregistrer avec succees');</script>");
 
